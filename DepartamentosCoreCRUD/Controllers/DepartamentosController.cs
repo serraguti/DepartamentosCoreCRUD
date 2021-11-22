c47f47029e1e03d5bb1819f7e995953e21205b5b
Serra <pacoserranox@gmail.com>
2021-11-22 19:23:39 +0100

    commit

--- DepartamentosCoreCRUD/Controllers/DepartamentosController.cs
+++ DepartamentosCoreCRUD/Controllers/DepartamentosController.cs
@@ -1,4 +1,5 @@
 ﻿using DepartamentosCoreCRUD.Data;
+using DepartamentosCoreCRUD.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,7 +19,46 @@ namespace DepartamentosCoreCRUD.Controllers
 
         public IActionResult Index()
         {
+            List<Departamento> listadepartamentos = this.context.GetDepartamentos();
+            return View(listadepartamentos);
+        }
+
+        public IActionResult Details(int iddepartamento)
+        {
+            Departamento departamento = this.context.FindDepartamento(iddepartamento);
+            return View(departamento);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int iddepartamento, string nombre, string localidad)
+        {
+            this.context.InsertDepartamento(iddepartamento, nombre, localidad);
+            //SI QUEREMOS ENVIAR A OTRA VISTA...
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int iddepartamento)
+        {
+            Departamento departamento = this.context.FindDepartamento(iddepartamento);
+            return View(departamento);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int iddepartamento, string nombre, string localidad)
+        {
+            this.context.UpdateDepartamento(iddepartamento, nombre, localidad);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int iddepartamento)
+        {
+            this.context.DeleteDepartamento(iddepartamento);
+            return RedirectToAction("Index");
         }
     }
 }
--- DepartamentosCoreCRUD/Startup.cs
+++ DepartamentosCoreCRUD/Startup.cs
@@ -54,7 +54,7 @@ namespace DepartamentosCoreCRUD
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Departamentos}/{action=Index}/{id?}");
             });
         }
     }
