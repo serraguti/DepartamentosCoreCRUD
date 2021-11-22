be0d929859ae7bc94d22084116a6ec1abf49aa8d
Serra <pacoserranox@gmail.com>
2021-11-22 17:30:49 +0100

    commit

---
+++ DepartamentosCoreCRUD/Controllers/DepartamentosController.cs
@@ -0,0 +1,24 @@
+﻿using DepartamentosCoreCRUD.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartamentosCoreCRUD.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        DepartamentosContext context;
+
+        public DepartamentosController(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DepartamentosCoreCRUD/Startup.cs
+++ DepartamentosCoreCRUD/Startup.cs
@@ -1,3 +1,4 @@
+using DepartamentosCoreCRUD.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,9 @@ namespace DepartamentosCoreCRUD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            String cadenaconexion = this.Configuration.GetConnectionString("cadenahospitallocal");
+            DepartamentosContext context = new DepartamentosContext(cadenaconexion);
+            services.AddTransient<DepartamentosContext>(contexto => context);
             services.AddControllersWithViews();
         }
 
